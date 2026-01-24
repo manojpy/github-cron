3e5a529bc1e8b896f82761124433ef195b231b40
manojpy <manojpy@gmail.com>
2026-01-24 09:55:03 +0530

    Add files via upload

--- src/macd_unified.py
+++ src/macd_unified.py
@@ -277,9 +277,6 @@ class BotConfig(BaseModel):
                 f'(minimum recommended: 200MB)'
             )
 
-        if self.RVOL_THRESHOLD < 0.5 or self.RVOL_THRESHOLD > 2.0:
-            errors.append(f'RVOL_THRESHOLD {self.RVOL_THRESHOLD} outside range [0.5, 2.0]')
-
         if self.MAX_CANDLE_STALENESS_SEC < 300:
             warnings.append(f'MAX_CANDLE_STALENESS_SEC very low ({self.MAX_CANDLE_STALENESS_SEC}s)')
 
