80a4d6b33afc7ecc8240046d9d20497e41918236
manojpy <manojpy@gmail.com>
2026-07-20 21:30:52 +0530

    Add files via upload

--- src/macd_unified.py
+++ src/macd_unified.py
@@ -162,7 +162,7 @@ class BotConfig(BaseModel):
     DEBUG_MODE: bool = Field(default=False, env='DEBUG_MODE')
     SEND_TEST_MESSAGE: bool = Field(default=True, description="Send test message on startup")
     BOT_NAME: str = "Unified Alert Bot"
-    PAIRS: List[str] = Field(default=["ETHUSD", "AVAXUSD", "XRPUSD", "BNBUSD", "LTCUSD", "DOTUSD", "ADAUSD", "SUIUSD", "AAVEUSD", "SOLUSD", "PAXGUSD", "PIPPINUSD", "RIVERUSD", "BLESSUSD", "BASEDUSD","SKYAIUSD","HUSD","EDENUSD","XAUTUSD", "ZECUSD", "LABUSD", "MOVEUSD", "LINKUSD", "ARBUSD", "KITEUSD", "VVVUSD", "BEATUSD", "BCHUSD", "BILLUSD", "WLDUSD" ], min_length=1) 
+    PAIRS: List[str] = Field(default=["ETHUSD", "AVAXUSD", "XRPUSD", "BNBUSD", "LTCUSD", "DOTUSD", "ADAUSD", "SUIUSD", "AAVEUSD", "SOLUSD", "PAXGUSD", "PIPPINUSD", "RIVERUSD", "BLESSUSD", "BASEDUSD","SKYAIUSD","HUSD","EDENUSD","XAUTUSD", "ZECUSD", "LABUSD", "BTCUSD", "LINKUSD", "ARBUSD", "KITEUSD", "VVVUSD", "BEATUSD", "BCHUSD", "BILLUSD", "WLDUSD" ], min_length=1) 
     PPO_FAST: int = Field(default=7, ge=1, le=50, description="PPO fast period")
     PPO_SLOW: int = Field(default=16, ge=2, le=100, description="PPO slow period")
     PPO_SIGNAL: int = Field(default=5, ge=1, le=25, description="PPO signal period")
